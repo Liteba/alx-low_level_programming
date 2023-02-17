f9e014bb785edbf66b2c64c4404784d4adf1cfc4
Liteba <litebaabua@gmail.com>
2023-02-16 18:34:02 -0800

    the man

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
-*main - Entry point
+*main - Exam paper
 *
 *Return: 1 when successfull
 */
 int main(void)
 {
-fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
