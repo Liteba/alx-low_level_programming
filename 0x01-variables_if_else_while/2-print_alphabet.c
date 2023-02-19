408d9d3abcdaee910c8388d110811bfe28c6753a
Liteba <litebaabua@gmail.com>
2023-02-19 15:04:37 -0800

    love

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+*main - main work
+*
+*Return: 0 if successfull
+*/
+
+int main(void)
+{
+char i;
+
+for (i = 'a' ; i <= 'z' ; i++)
+putchar(i);
+putchar('\n');
+return (0);
+}
