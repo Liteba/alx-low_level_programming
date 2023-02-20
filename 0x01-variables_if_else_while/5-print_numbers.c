5d5a9a6f29bb9c5b7b274cf430f0ac75ca9bcfb8
Liteba <litebaabua@gmail.com>
2023-02-19 16:07:53 -0800

    plkh

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+*main - main work
+*
+*Return: 0 if successfull
+*/
+
+int main(void)
+{
+int i;
+
+for(i = 0 ; i < 10 ; i++);
+printf("%d", i);
+printf("\n");
+return (0);
+}
+
