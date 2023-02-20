074aa825d018b9b50e8aafdc66da557ecfc4e55c
Liteba <litebaabua@gmail.com>
2023-02-20 13:12:08 -0800

    kojo

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+*main - main work
+*
+*Return: 0 if successfull
+*/
+
+int main(void)
+{
+int i;
+
+for (i = 'z' ; i >= 'a' ; i--)
+putchar(i);
+putchar('\n');
+return (0);
+}
+
