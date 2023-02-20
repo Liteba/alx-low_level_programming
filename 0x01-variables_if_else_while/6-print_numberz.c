8e4de532ec55fd00ab4a999e39f5d27acb7ae40a
Liteba <litebaabua@gmail.com>
2023-02-20 12:27:39 -0800

    lop

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+*main - main work
+*
+*Return: 0 if successfull
+*/
+
+int main(void)
+{
+int i;
+
+for(i = 0 ; i < 10 ; i++)
+putchar(i + '0');
+putchar('\n');
+return (0);
+}
